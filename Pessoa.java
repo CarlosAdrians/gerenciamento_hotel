4578933d3b2393524af3552794ab6e9132419456
carlos adrians <carlosadrians09@gmail.com>
2024-08-26 20:24:07 -0300

    dadadadadadda

---
+++ Pessoa.java
@@ -0,0 +1,24 @@
+public class Pessoa {
+    
+    protected String nome;
+    protected int cpf;
+    public Pessoa(String nome, int cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
